83bf51c219db6b35492449e48c564b4976520b47
Aadya Anil Kumar <85759627+Aadya-Anil@users.noreply.github.com>
2025-12-12 16:26:28 +0530

    added comments for clarity

--- src/puzzle_generator.py
+++ src/puzzle_generator.py
@@ -1,36 +1,28 @@
-# src/puzzle_generator.py
-
 import random
 
 def generate_puzzle(level):
-    """
-    Returns (expression_string, correct_answer)
-    Difficulty grows mainly by expression complexity.
-    """
-
-    if level == 0:
-        # Easy: one operation
+    
+    if level == 0:  # Easy level
         a, b = random.randint(1, 9), random.randint(1, 9)
-        op = random.choice(["+", "-"])
-        if op == "-" and b > a:
+        op = random.choice(["+", "-","*","/"])
+        if op == "-" and b > a: # to avoid negative numbers because this is for ages 5-10
             a, b = b, a
         expr = f"{a} {op} {b}"
 
-    elif level == 1:
-        # Medium: + or - with 2 steps
+    elif level == 1: # Medium : longer expressions
         a, b, c = random.randint(1, 9), random.randint(1, 9), random.randint(1, 9)
-        expr = f"{a} + {b} - {c}"
+        op = random.choice(["+", "-"])
+        expr = f"{a} {op} {b} {op} {c}"
 
-    elif level == 2:
-        # Hard: includes multiplication
+    elif level == 2: # Hard: much longer expressions , includes multiplication and division
         a, b, c = random.randint(1, 9), random.randint(1, 9), random.randint(1, 9)
-        expr = f"{a} * {b} + {c}"
+        op = random.choice(["+", "-","*","/"])
+        expr = f"{a} {op} {b} {op} {c}"
 
-    else:
-        # Level 3: two small expressions combined
+    else: # Warrior evel : combines two small expressions 
         a, b = random.randint(1, 9), random.randint(1, 9)
         c, d = random.randint(1, 9), random.randint(1, 9)
-        expr = f"({a} + {b}) * ({c} - 1)"
+        expr = f"({a} + {b}) * ({c} - {d})"
 
     ans = eval(expr)
     return expr, ans
